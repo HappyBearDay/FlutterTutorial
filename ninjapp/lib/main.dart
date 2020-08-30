5e7aed976bed1f2efdad632d0cca0b21c8451523
HappyBearDay <anishamroun2@gmail.com>
2020-08-31 00:50:08 +0200

    chapter 13

--- ninjapp/lib/main.dart
+++ ninjapp/lib/main.dart
@@ -14,27 +14,28 @@ class Home extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.red[600],
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        crossAxisAlignment: CrossAxisAlignment.end,
+      body: Row(
         children: [
-          Row(
-            children: [Text("hello"), Text(" word"),],
-          ),
-          Container(
-            padding: EdgeInsets.all(20.0),
-            color: Colors.cyan,
-            child: Text("one"),
-          ),
-          Container(
-            padding: EdgeInsets.all(30.0),
-            color: Colors.pinkAccent,
-            child: Text("two"),
-          ),
-          Container(
-            padding: EdgeInsets.all(40.0),
-            color: Colors.amber,
-            child: Text("three"),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Container(
+                padding: EdgeInsets.all(20.0),
+                color: Colors.cyan,
+                child: Text("one"),
+              ),
+              Container(
+                padding: EdgeInsets.all(30.0),
+                color: Colors.pinkAccent,
+                child: Text("two"),
+              ),
+              Container(
+                padding: EdgeInsets.all(40.0),
+                color: Colors.amber,
+                child: Text("three"),
+              ),
+            ],
           ),
         ],
       ),
