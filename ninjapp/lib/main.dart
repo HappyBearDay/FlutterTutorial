91f87948c01cadfba885ebfec2331decce429673
HappyBearDay <anishamroun2@gmail.com>
2020-08-30 16:42:01 +0200

    chapter 8

--- ninjapp/lib/main.dart
+++ ninjapp/lib/main.dart
@@ -15,19 +15,10 @@ class Home extends StatelessWidget {
         backgroundColor: Colors.red[600],
       ),
       body: Center(
-        child: Text(
-          "hello ninjas !",
-          style: TextStyle(
-              fontSize: 20.0,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 2.0,
-              color: Colors.grey[600],
-              fontFamily: "IndieFlower"
-          ),
-        ),
+        child: Image.asset("assets/space-3.jpg")
       ),
       floatingActionButton: FloatingActionButton(
-        child: Text("click me!"),
+        child: Text("click"),
         backgroundColor: Colors.red[600],
       ),
     );
