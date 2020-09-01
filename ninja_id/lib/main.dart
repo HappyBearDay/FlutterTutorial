7b03ca98f9ca598e56238a7f54f7a38a760c632f
HappyBearDay <anishamroun2@gmail.com>
2020-09-02 00:49:35 +0200

    Chapter 16

--- ninja_id/lib/main.dart
+++ ninja_id/lib/main.dart
@@ -6,7 +6,14 @@ void main() {
   ));
 }
 
-class NinjaCard extends StatelessWidget {
+class NinjaCard extends StatefulWidget {
+  @override
+  _NinjaCardState createState() => _NinjaCardState();
+}
+
+class _NinjaCardState extends State<NinjaCard> {
+  int ninjaLevel = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,6 +23,14 @@ class NinjaCard extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.grey[850],
         elevation: 0.0,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          setState(() {
+            ninjaLevel +=1;
+          });
+          },
+        child: Icon(Icons.add),
       ),
       body: Padding(
         padding: EdgeInsets.fromLTRB( 30.0, 40.0, 30.0, 0.0),
@@ -59,7 +74,7 @@ class NinjaCard extends StatelessWidget {
             ),
             SizedBox(height: 10.0),
             Text(
-              "8",
+              "$ninjaLevel",
               style: TextStyle(
                   color: Colors.amberAccent[200],
                   letterSpacing: 2.0,
@@ -91,3 +106,5 @@ class NinjaCard extends StatelessWidget {
     );
   }
 }
+
+
