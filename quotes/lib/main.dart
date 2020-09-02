c84c042daa9e26c894dcc6179beaa4e0a6ca6624
HappyBearDay <anishamroun2@gmail.com>
2020-09-02 20:38:26 +0200

    Chapter 18

--- quotes/lib/main.dart
+++ quotes/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import "quote.dart";
 void main() {
   runApp(MaterialApp(
     home: QuotesList(),
@@ -11,11 +11,13 @@ class QuotesList extends StatefulWidget {
 }
 
 class _QuotesListState extends State<QuotesList> {
-  List<String> quotes=[
-    'Be yourself; everyone else is already taken',
-    'I have nothing to declare except my genius',
-    'The truth is rarely pure and never simple'
+  List<Quote> quotes=[
+    Quote(author: "Oscar Wilde", text:'Be yourself; everyone else is already taken'),
+    Quote(author: "Oscar Wilde", text:'I have nothing to declare except my genius'),
+    Quote(author: "Oscar Wilde", text:'The truth is rarely pure and never simple'),
   ];
+
+  List<String> authors = [];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,7 +28,7 @@ class _QuotesListState extends State<QuotesList> {
         backgroundColor: Colors.redAccent,
       ),
       body: Column(
-        children: quotes.map((quote) => Text(quote)).toList(),
+        children: quotes.map((quote) => Text("${quote.author} - ${quote.text}")).toList(),
       ),
     );
   }
---
+++ quotes/lib/quote.dart
@@ -0,0 +1,8 @@
+import 'dart:io';
+
+class Quote{
+  String text;
+  String author;
+
+  Quote({this.text, this.author});
+}
